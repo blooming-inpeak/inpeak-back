43c659c085582a34011c711a00da8e4582a6934a
minwooKang <rkdalsdn94@gmail.com>
2025-07-04 20:40:48 +0900

    refactor: 사용하지 않는 코드 정리
    Redis 관련 로직 삭제
    자동 닉네임 생성 동물 변경
    gitignore 파일 위치 변경

--- inpeak/src/test/java/com/blooming/inpeak/support/IntegrationTestSupport.java
+++ inpeak/src/test/java/com/blooming/inpeak/support/IntegrationTestSupport.java
@@ -1,12 +1,9 @@
 package com.blooming.inpeak.support;
 
-import com.blooming.inpeak.TestRedisConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@Import(TestRedisConfiguration.class)
 public abstract class IntegrationTestSupport {
 }
