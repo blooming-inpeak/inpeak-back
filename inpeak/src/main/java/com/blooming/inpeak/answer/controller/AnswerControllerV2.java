341ddc8cd0b9a6ff3d5fac0e92d09bb21b9565ff
minwooKang <rkdalsdn94@gmail.com>
2025-08-10 16:43:37 +0900

    chore: ECR cicd - v12 (#2)

--- inpeak/src/main/java/com/blooming/inpeak/answer/controller/AnswerControllerV2.java
+++ inpeak/src/main/java/com/blooming/inpeak/answer/controller/AnswerControllerV2.java
@@ -25,7 +25,6 @@ public class AnswerControllerV2 {
     private final AnswerService answerService;
     private final AnswerAsyncService answerAsyncService;
 
-    // TODO: ECR cicd를 위한 주석 추가
     @PostMapping("/create")
     public ResponseEntity<Long> createAnswer(
         @AuthenticationPrincipal MemberPrincipal memberPrincipal,
